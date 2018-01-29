4d7cf92625ee9d5fe11360dab0823219a2bfa567
mpetterson <mpetterson@mpetterson.allaria.local>
2018-01-29 13:56:56 -0300

    Commit 2018/01/29 Login cambios

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -19,98 +19,56 @@
 			<!--<link href="https://fonts.googleapis.com/css?family=Quicksand|Slabo+27px" rel="stylesheet">-->
 		</head>
 		<body>
-			<div id="login">
-				<header>Calculadora de Bonos</header>
-				<form id="form">
-                                    <input type="text" id="nombreUsuario" placeholder="usuario">
-                                    <input type="password" id="clave" placeholder="clave">
-                                    
-                                    
-                                    <select id="dominio">
-                                        <?php    
-                                        $dominios = DOMINIOS; 
-                                        $dominios = str_replace('"', "", $dominios);
-                                        $arrayDominios = explode(",", $dominios);
-                                        foreach($arrayDominios as $dominio){ ?>
-                                            <option value="<?= $dominio; ?>"><?php echo $dominio; ?></option>
-                                        <?php } ?>
-                                    </select>
-                                    
-                                    <button id="aceptar">Ingresar</button>
-                                    
-				</form>
-			</div>
+                    <div id="login">
+                        <div>
+                            <header>Calculadora de Bonos</header>
+                            <form id="form">
+                                <input type="text" id="nombreUsuario" placeholder="usuario">
+                                <input type="password" id="clave" placeholder="clave">
+                                <select id="dominio">
+                                    <?php    
+                                    $dominios = DOMINIOS; 
+                                    $dominios = str_replace('"', "", $dominios);
+                                    $arrayDominios = explode(",", $dominios);
+                                    foreach($arrayDominios as $dominio){ ?>
+                                        <option value="<?= $dominio; ?>"><?php echo $dominio; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </form>
+
+                        </div>   
+
+                        <div>
+                            <button id="aceptar">Ingresar</button>
+                        </div>
+
+                    </div>
 		</body>
 	</html>
 
 <script>
     
-    $(document).ready(function() {
+//    $(document).ready(function() {
+$(function(){
         $("body").data('theme', 'darkblue');
         
         var theme = getTheme();
         var formOK = false;
-            
-            
-//        <select id="dom">
-//                <option>Contact name</option>
-//        </select>//      
-            
-//        $('#ventanaLogin').jqxWindow({       
-//            height: 170, 
-//            width: 270,
-//            theme: theme, 
-//            resizable: false, 
-//            isModal: true, 
-//            modalOpacity: 0.3,
-//            autoOpen: true,
-//            initContent: function () {
-//                $('#aceptar').jqxButton({ theme: theme, width: '65px' });
-//                $('#formLogin').jqxValidator({
-//                    rules: [
-//                        { input: '#nombreUsuario', message: 'Debe ingresar el nombre de usuario!', rule: 'required' },
-//                        { input: '#clave', message: 'Debe ingresar la clave!',  rule: 'required' }
-//                    ], theme: theme
-//                });
-//            }
-//        });
-        
-        
-        
+
         $('#form').jqxValidator({ rules: [
                     { input: '#nombreUsuario', message: 'Debe ingresar el nombre de usuario!', rule: 'required' },
                     { input: '#clave', message: 'Debe ingresar la clave!',  rule: 'required' }
-                    
                     ], 
                     theme: theme
         });
-        
-        
-//        $("#nombreUsuario").jqxInput({placeHolder: "Usuario", height: '20px', width: '170px', minLength: 1, theme: theme });
-//        $("#clave").jqxPasswordInput({  width: '170px', height: '20px', theme: theme});
 
-
-//        var sourceDominios = [<?php echo DOMINIOS;?>];
-//        $("#dominio").jqxDropDownList({ 
-//            source: sourceDominios, 
-//            selectedIndex: 0, 
-//            width: '170', 
-//            height: '30px', 
-//            theme: theme});
-//        if ($("#dominioPrevio").val()){
-//            $("#dominio").jqxDropDownList('selectItem', $("#dominioPrevio").val());
-//        }
-        
-//        $('#form').bind('validationSuccess', function (event) { formOK = true; });
-//        $('#form').bind('validationError', function (event) { formOK = false; }); 
-//        
         $('#form').bind('validationSuccess', function (event) { formOK = true; });
         $('#form').bind('validationError', function (event) { formOK = false; }); 
         
         $('#aceptar').bind('click', function () {
             $('#form').jqxValidator('validate');
             if (formOK){
-                console.log('OK');
+                $('#login').ajaxloader();
                 datos = {
                     nombreUsuario: $('#nombreUsuario').val(),
                     clave: $('#clave').val(),
@@ -118,18 +76,16 @@
                 };
                 $.post('usuario/validarUsuario', datos, function(data){
                     if (data.resultado == 'OK'){
-                        console.log('OK');
                         $(location).attr('href','/calculadora');
-//                        $.redirect('/calculadora/resultado');
                     } else {
                         new Messi(data.resultado, {title: 'Error', 
                             buttons: [{id: 0, label: 'Cerrar', val: 'X'}], modal:true});
+                        $('#login').ajaxloader('hide');
                     }
                 }, 'json');
             }
         });        
-        
-        
+
         $('input').keypress(function (e) {
             if(e.which === 13) {
                 $('#aceptar').click();
